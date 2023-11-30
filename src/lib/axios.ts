03d271a470b72121915bb16b4e1b9ce4835fb8a8
Ana Carla <anacarlan07@gmail.com>
2023-11-30 11:29:10 -0300

    fix: teste commit

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: "https://invoice-app-iewu.onrender.com/api/", //change to your API URL here
+	baseURL: "https://invoice-app-iewu.onrender.com/api/", 
 });
