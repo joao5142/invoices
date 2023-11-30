2a2aabbfea368fe54609ac638cd03dabdae736a9
joao5142 <joaopauloneto3687@gmail.com>
2023-11-30 15:40:29 -0300

    refactor:adjusting build erros

--- src/components/layouts/Aside/index.tsx
+++ src/components/layouts/Aside/index.tsx
@@ -1,4 +1,11 @@
-import { AsideContainer, Avatar, DialogBackground, DialogContent, DialogInfo, Logo } from "./styles";
+import {
+  AsideContainer,
+  Avatar,
+  DialogBackground,
+  DialogContent,
+  DialogInfo,
+  Logo,
+} from "./styles";
 import Image from "next/image";
 import logoImg from "@/assets/images/logo.svg";
 import { useTheme } from "styled-components";
@@ -7,58 +14,74 @@ import { AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import { createPortal } from "react-dom";
 
-import avatarImg from "@/assets/images/avatar.PNG";
+import avatarImg from "@/assets/images/avatar.png";
 
 export function Aside() {
-	const [isDialogOpen, setIsDialogOpen] = useState(false);
-	const { icon } = useTheme();
-	const { toggleTheme } = useToggleTheme();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const { icon } = useTheme();
+  const { toggleTheme } = useToggleTheme();
 
-	function handleToggleTheme() {
-		toggleTheme();
-	}
+  function handleToggleTheme() {
+    toggleTheme();
+  }
 
-	function handleCloseDialogInfo() {
-		setIsDialogOpen(false);
-	}
-	function handleOpenDialogInfo() {
-		setIsDialogOpen(true);
-	}
-	return (
-		<AsideContainer>
-			<header>
-				<Logo>
-					<Image width={40} src={logoImg} alt="" />
-				</Logo>
-			</header>
-			<footer>
-				<div>
-					<Image onClick={handleToggleTheme} width={25} height={25} src={icon.path} alt="" />
-				</div>
-				<div>
-					<Avatar onClick={handleOpenDialogInfo} width={40} height={40} src={avatarImg} alt="" />
-				</div>
-			</footer>
-			{isDialogOpen &&
-				createPortal(
-					<AnimatePresence mode="popLayout">
-						<DialogInfo initial={{ opacity: 0.5 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-							<DialogBackground onClick={handleCloseDialogInfo} />
-							<DialogContent>
-								<Avatar width={130} height={130} src={avatarImg} alt="" />
-								<strong>João Paulo</strong>
+  function handleCloseDialogInfo() {
+    setIsDialogOpen(false);
+  }
+  function handleOpenDialogInfo() {
+    setIsDialogOpen(true);
+  }
+  return (
+    <AsideContainer>
+      <header>
+        <Logo>
+          <Image width={40} src={logoImg} alt="" />
+        </Logo>
+      </header>
+      <footer>
+        <div>
+          <Image
+            onClick={handleToggleTheme}
+            width={25}
+            height={25}
+            src={icon.path}
+            alt=""
+          />
+        </div>
+        <div>
+          <Avatar
+            onClick={handleOpenDialogInfo}
+            width={40}
+            height={40}
+            src={avatarImg}
+            alt=""
+          />
+        </div>
+      </footer>
+      {isDialogOpen &&
+        createPortal(
+          <AnimatePresence mode="popLayout">
+            <DialogInfo
+              initial={{ opacity: 0.5 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <DialogBackground onClick={handleCloseDialogInfo} />
+              <DialogContent>
+                <Avatar width={130} height={130} src={avatarImg} alt="" />
+                <strong>João Paulo</strong>
 
-								<a href="" target="_blank">
-									Github Link
-								</a>
-								<a href="" target="_blank">
-									Deploy Link
-								</a>
-							</DialogContent>
-						</DialogInfo>
-					</AnimatePresence>,
-					document.body
-				)}
-		</AsideContainer>
-	);
+                <a href="" target="_blank">
+                  Github Link
+                </a>
+                <a href="" target="_blank">
+                  Deploy Link
+                </a>
+              </DialogContent>
+            </DialogInfo>
+          </AnimatePresence>,
+          document.body
+        )}
+    </AsideContainer>
+  );
 }
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,35 +1,52 @@
 import { format, add, parse } from "date-fns";
 
 function formatStringToDate(stringDate: string) {
-  const [day, month, year] = stringDate.split("/");
+  try {
+    const [day, month, year] = stringDate.split("/");
 
-  let date = new Date(Number(year), Number(month) - 1, Number(day));
+    let date = new Date(Number(year), Number(month) - 1, Number(day));
 
-  return date;
+    return date;
+  } catch (err) {
+    console.log(err);
+  }
+  return new Date();
 }
 export function formatDate(stringDate: string) {
-  if (stringDate) {
-    let date = formatStringToDate(stringDate);
-    return format(date, "dd MMM yyyy");
+  try {
+    if (stringDate) {
+      console.log(stringDate);
+      let date = formatStringToDate(stringDate) as Date;
+      return format(date, "dd MMM yyyy");
+    }
+  } catch (err) {
+    console.log(err);
   }
-
   return "";
 }
 export function addDaysToDate(stringDate: string, days: string | number) {
-  if (stringDate) {
-    let date = formatStringToDate(stringDate);
+  try {
+    if (stringDate) {
+      let date = formatStringToDate(stringDate) as Date;
 
-    const newDate = add(date, { days: Number(days) });
+      const newDate = add(date, { days: Number(days) });
 
-    return format(newDate, "dd MMM yyyy");
+      return format(newDate, "dd MMM yyyy");
+    }
+  } catch (err) {
+    console.log(err);
   }
   return "";
 }
 
 export function formatDateToUTCFormat(stringDate: string) {
-  if (stringDate) {
-    const parsedDate = parse(stringDate, "dd/MM/yyyy", new Date());
-    return format(parsedDate, "yyyy-MM-dd");
+  try {
+    if (stringDate) {
+      const parsedDate = parse(stringDate, "dd/MM/yyyy", new Date());
+      return format(parsedDate, "yyyy-MM-dd");
+    }
+  } catch (err) {
+    console.log(err);
   }
   return "";
 }
